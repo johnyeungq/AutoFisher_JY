c659dccdbaf5e90708593a0319ef10ff9ed68092
Yeung John <johnyeungq@gmail.com>
2025-06-30 15:20:19 +0800

    commit from company, build test at home tonight

--- AutoFisherConfig.cs
+++ AutoFisherConfig.cs
@@ -8,6 +8,15 @@ namespace AutoFisher_JY
     {
         public override ConfigScope Mode => ConfigScope.ClientSide;
 
+        [Label("Enable Auto Fisher")]
+        [DefaultValue(true)]
+        public bool EnableAutoFisher { get; set; }
+        [Label("Toggle Auto-Fisher Keybind")]
+        [Tooltip("Select a key (1-9) to bind for toggling Auto-Fisher.")]
+        [DefaultValue(1)]
+        [Range(1, 9)]
+        public int ToggleAutoFisherKey { get; set; }
+
       
     }
 }
